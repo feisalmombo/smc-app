b6b069a6758ac83b816968f32cd4e376f47c3f5d
feisalmombo <feisalmombo29@gmail.com>
2024-09-13 12:13:48 +0300

    WIP

--- contact.php
+++ contact.php
@@ -18,6 +18,33 @@
             <a href="membership.php">Membership</a>
         </nav>
     </header>
+
+    <?php
+        require('db.php');
+        // If form submitted, insert values into the database.
+        if (isset($_REQUEST['surname'])){
+            $first_name = stripslashes($_REQUEST['first_name']); // removes backslashes
+            $first_name = mysqli_real_escape_string($con,$first_name); //escapes special characters in a string
+
+            $surname = stripslashes($_REQUEST['surname']); // removes backslashes
+            $surname = mysqli_real_escape_string($con,$surname); //escapes special characters in a string
+            
+            $email = stripslashes($_REQUEST['email']);
+            $email = mysqli_real_escape_string($con,$email);
+
+            $role = stripslashes($_REQUEST['role']);
+            $role = mysqli_real_escape_string($con,$role);
+            
+            $password = stripslashes($_REQUEST['password']);
+            $password = mysqli_real_escape_string($con,$password);
+            
+            $query = "INSERT into `users` (first_name, surname, email, password, role) VALUES ('$first_name', '$surname', '$email', '".md5($password)."', '$role')";
+            $result = mysqli_query($con,$query) or die(mysqli_error($con));
+            if($result){
+                echo "<div class='form'><h3>You are registered successfully.</h3><br/>Click here to <a href='login.php'>Login</a></div>";
+            }
+        }else{
+    ?>
     
     <main>
         <h2>Contact Us</h2>
@@ -26,11 +53,11 @@
         <p>Phone: +123 456 7890</p>
         <form action="contact.php" method="post">
             <label for="name">Name:</label><br>
-            <input type="text" id="name" name="name"><br><br>
+            <input type="text" id="name" name="name" required><br><br>
             <label for="email">Email:</label><br>
-            <input type="email" id="email" name="email"><br><br>
+            <input type="email" id="email" name="email" required><br><br>
             <label for="message">Message:</label><br>
-            <textarea id="message" name="message"></textarea><br><br>
+            <textarea id="message" name="message" required></textarea><br><br>
             <button type="submit">Send Message</button>
         </form>
     </main>
@@ -38,6 +65,7 @@
     <footer>
         <p>&copy; 2024 Social Media Campaigns Ltd.</p>
     </footer>
+<?php } ?>
 </body>
 </html>
 
